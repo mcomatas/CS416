8dbad0b0e1873bdc1c1ecb8f25f185aa3217e0ac
Michael A. Comatas <mac776@cd.cs.rutgers.edu>
2020-09-15 15:51:09 -0400

    TimeSysCall.c finished

--- TimeSysCall.c
+++ TimeSysCall.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <sys/time.h>
+
 // Place any necessary global variables here
+
+//double startTime, endTime;
 
 int main(int argc, char *argv[]){
 
@@ -11,6 +15,31 @@ int main(int argc, char *argv[]){
 	 *
 	 *
 	 */
+
+    //Author: Michael Comatas
+
+    struct timeval currTime;
+    double startTime, endTime;
+
+    int i = 0;
+    //startTime = gettimeofday( &currTime, NULL );
+    gettimeofday( &currTime, NULL );
+    startTime = currTime.tv_usec;
+    while( i < 100000 )
+    {
+        getpid();
+        i++;
+    }
+    //endTime = gettimeofday( &currTime, NULL );
+    gettimeofday( &currTime, NULL );
+    endTime = currTime.tv_usec;
+
+    double totalTime = endTime - startTime;
+    double avgTime = totalTime / 100000;
+
+    printf( "Syscalls Performed: %d\n", i );
+    printf( "Total Elapsed Time: %f microseconds\n", totalTime );
+    printf( "Average Time Per Syscall: %f microseconds\n", avgTime );
 
 	return 0;
 
